458a5b7f383a1f81283935553b61c5af3b24364b
Lucas Fortune <lucas.fortune@web.de>
2023-03-07 15:12:56 +0100

    Added basic color synthesis concept
    Blue wave generator

--- videosynth.py
+++ videosynth.py
@@ -8,15 +8,28 @@ img = np.ones((h, w, 3), np.uint8)
 
 #noise generator
 def noise_gen():
-    x=np.random.randint(0, 480, 10000)
-    y=np.random.randint(0, 480, 10000)
+    '''
+    Simple noise generator function
+        Paramters:
+        Returns:
+    '''
+    x=np.random.randint(0, 480, 100)
+    y=np.random.randint(0, 480, 100)
     b=np.random.randint(0,255)
     g=np.random.randint(0,255)
     r=np.random.randint(0,255)
     img[x, y, :]=[b, g, r]
 
+#blue wave generator
+counter_blue=0
+def blue_wf():
+    global counter_blue
+    img[0:counter_blue, :, 0]+=1
+    counter_blue+=1
+    
 while True:
-    noise_gen()
+    #noise_gen()
+    blue_wf()
     cv2.imshow('image', img)
     c = cv2.waitKey(1)
     if c & 0xFF == ord("q"):
