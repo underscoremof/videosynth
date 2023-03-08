aa690a3e08842a83c9c2389a7e7087a84c2ac7df
Lucas Fortune <lucas.fortune@web.de>
2023-03-08 11:30:08 +0100

    Added faux feedback function
    not actual video feedback, just combination of rotating and filtering

--- videosynth.py
+++ videosynth.py
@@ -5,7 +5,7 @@ import cv2
 starting image; resolution hxw
 '''
 h = 480 
-w = 480 
+w = 720 
 img = np.ones((h, w, 3), np.uint8)
 
 f=0
@@ -32,8 +32,9 @@ def noise_gen():
         Returns:
     To do: add modulation
     '''
-    x=np.random.randint(0, 480, 100)
-    y=np.random.randint(0, 480, 100)
+    global h, w
+    x=np.random.randint(0, h, 10)
+    y=np.random.randint(0, w, 10)
     b=np.random.randint(0,255)
     g=np.random.randint(0,255)
     r=np.random.randint(0,255)
@@ -59,31 +60,52 @@ def blue_wf(speed=5):
         img[counter_blue_bottom:479, :, 0]-=abs(speed)
         counter_blue_bottom-=1
 
+''' Parameters for opencv rotation matrix function'''
+(h, w) = img.shape[:2]
+(cX, cY) = (w // 2, h // 2)
+angle = 180
+zoom = 1.01
+'''Filter kernel for video feedback simulation'''
+fb_ker_outside = 0.11
+fb_ker_center = -10
+feedback_kernel = np.array(
+    [[fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside],
+     [fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside],
+     [fb_ker_outside, fb_ker_outside, fb_ker_center, fb_ker_outside, fb_ker_outside],
+     [fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside],
+     [fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside]])
+
+def faux_feedback():
+    '''
+    Combination of rotation and filtering image simulating analog camera feedback
+        Parameters:
+        Returns:
+    '''
+    global img
+    M = cv2.getRotationMatrix2D((cX, cY), angle, zoom)
+    rotated = cv2.warpAffine(img, M, (w, h))
+    img = rotated
+    img = cv2.filter2D(img, -1, feedback_kernel)
+
 def reset():
     '''
-    reset function to return to black image
+    reset function to return to black image and start over synthesis
     '''
     global img, counter_blue_bottom, counter_blue_top, f
-    #reset image
     img = np.ones((h, w, 3), np.uint8)
-    #reset blue wave function
     counter_blue_top=0
     counter_blue_bottom=479
-    #reset sine wave
     f=0
-
 reset()
-    
+
 while True:
-    cv2.imshow('image', img)
     noise_gen()
     #blue_wf()
-
-    #cv2.imshow('image', img)
-    c = cv2.waitKey(1)
+    faux_feedback()
+    cv2.imshow('image', img)
+    c = cv2.waitKey(10)
     if c & 0xFF == ord("q"):
         cv2.destroyAllWindows()
         break
     if c & 0xFF == ord("r"):
         reset()
-
