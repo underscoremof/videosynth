3021bc5d13693d2f05d2fea545f613bfdedd599e
Lucas Fortune <lucas.fortune@web.de>
2023-03-08 12:02:36 +0100

    improved faux feedback function
    works so much better without the filter

--- videosynth.py
+++ videosynth.py
@@ -12,7 +12,7 @@ f=0
 def sinwave(freq=0.03, ampl=255):
     '''
     Simple sine wave function
-    can be used to modulate color intensity
+    can be used to modulate color intensity?
         Parameters: 
                 freq (float): Frequency of sine wave; usefull values between 0.001 and 0.1?
                 ampl (int): Amlitude of sine wave; should be between 0 and 255
@@ -33,8 +33,8 @@ def noise_gen():
     To do: add modulation
     '''
     global h, w
-    x=np.random.randint(0, h, 10)
-    y=np.random.randint(0, w, 10)
+    x=np.random.randint(0, h, 100)
+    y=np.random.randint(0, w, 100)
     b=np.random.randint(0,255)
     g=np.random.randint(0,255)
     r=np.random.randint(0,255)
@@ -42,7 +42,7 @@ def noise_gen():
 
 counter_blue_top=0
 counter_blue_bottom=479
-def blue_wf(speed=5):
+def blue_wf(speed=1):
     '''
     Creates blue waves flowing over the image
         Paramters:
@@ -60,11 +60,6 @@ def blue_wf(speed=5):
         img[counter_blue_bottom:479, :, 0]-=abs(speed)
         counter_blue_bottom-=1
 
-''' Parameters for opencv rotation matrix function'''
-(h, w) = img.shape[:2]
-(cX, cY) = (w // 2, h // 2)
-angle = 180
-zoom = 1.01
 '''Filter kernel for video feedback simulation'''
 fb_ker_outside = 0.11
 fb_ker_center = -10
@@ -74,18 +69,24 @@ feedback_kernel = np.array(
      [fb_ker_outside, fb_ker_outside, fb_ker_center, fb_ker_outside, fb_ker_outside],
      [fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside],
      [fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside, fb_ker_outside]])
+#img = cv2.filter2D(img, -1, feedback_kernel)
 
-def faux_feedback():
+''' Parameters for opencv rotation matrix function'''
+(h, w) = img.shape[:2]
+(cX, cY) = (w // 2, h // 2)
+def faux_feedback(angle=1, zoom=1.05):
     '''
     Combination of rotation and filtering image simulating analog camera feedback
-        Parameters:
+        Parameters: 
+                angle (int): Angle of rotation
+                zoom (float): zoom in (or out)
         Returns:
+    To do: add modulation
     '''
     global img
     M = cv2.getRotationMatrix2D((cX, cY), angle, zoom)
     rotated = cv2.warpAffine(img, M, (w, h))
     img = rotated
-    img = cv2.filter2D(img, -1, feedback_kernel)
 
 def reset():
     '''
@@ -101,7 +102,7 @@ reset()
 frame_counter = 0
 while True:
     noise_gen()
-    #blue_wf()
+    blue_wf()
     faux_feedback()
     if frame_counter % 2 == 0:
         cv2.imshow('image', img)
